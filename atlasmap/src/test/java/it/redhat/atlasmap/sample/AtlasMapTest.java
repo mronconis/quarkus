9322a0c78d605eb97f042f1fe6a76d894e78069b
Marco Ronconi <mronconi@redhat.com>
2022-03-09 15:45:23 +0100

    Add elementFormDefault="qualified" attribute to schema set

--- atlasmap/src/test/java/it/redhat/atlasmap/sample/AtlasMapTest.java
+++ atlasmap/src/test/java/it/redhat/atlasmap/sample/AtlasMapTest.java
@@ -18,6 +18,7 @@ public class AtlasMapTest {
 
     String source = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Person xmlns:ns=\"foo\"><ns:name>Mario</ns:name><ns:surname>Rossi</ns:surname><ns:gender>MAN</ns:gender></Person>";
     String exptectedTarget = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><envelop xmlns:cus=\"it.redhat.atlasmap.sample/customer\"><cus:customer><cus:name>Mario</cus:name></cus:customer></envelop>";
+    String exptectedTargetDefaultNs = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><envelop xmlns=\"it.redhat.atlasmap.sample/env\"><customer xmlns=\"it.redhat.atlasmap.sample/customer\"><name>Mario</name></customer></envelop>";
     
     @Test
     public void mappingFromXmlSchemaTest() throws Exception {
@@ -29,11 +30,11 @@ public class AtlasMapTest {
 
         session.setSourceDocument("person", source);
         context.process(session);
-        String target = (String) session.getTargetDocument("customer-schemaset-33417d3a-c092-4cc0-a8ff-fbe8484447b4");
+        String target = (String) session.getTargetDocument("customer-schemaset-3f6c7792-dfba-4652-b9ea-c2762d5a8e14");
 
         log.info("Target document from schema: " + target);
 
-        assertEquals(exptectedTarget, target);
+        assertEquals(exptectedTargetDefaultNs, target);
     }
 
     @Test
@@ -49,23 +50,6 @@ public class AtlasMapTest {
         String target = (String) session.getTargetDocument("customer-instance-7da3fc84-6e6b-42cc-96a9-bac5f17dd83b");
 
         log.info("Target document from instance: " + target);
-
-        assertEquals(exptectedTarget, target);
-    }
-
-    @Test
-    public void mappingFromXmlSchemaModifiedTest() throws Exception {
-
-        URL url = Thread.currentThread().getContextClassLoader().getResource("mapping/atlasmapping-from-xml-schema-mod.adm");
-        AtlasContextFactory factory = DefaultAtlasContextFactory.getInstance();
-        AtlasContext context = factory.createContext(url.toURI());
-        AtlasSession session = context.createSession();
-
-        session.setSourceDocument("person", source);
-        context.process(session);
-        String target = (String) session.getTargetDocument("customer-schemaset-b9fe20ef-e634-4628-9fbe-433df42761ab");
-
-        log.info("Target document from schema modified: " + target);
 
         assertEquals(exptectedTarget, target);
     }
